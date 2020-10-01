038c4ae41141f83ccaf6cdcbf7403f9091d21a13
0HYANGH0 <71264983+0HYANGH0@users.noreply.github.com>
2020-10-01 12:40:30 +0200

    Add files via upload

---
+++ contrasena.py
@@ -0,0 +1,7 @@
+'''escribe un programa que genere'''
+def contrasena():
+    nombre=raw_input("introduce el nombre: ")
+    apellido=raw_input("intrduce el apellido: ")
+    print nombre,"",apellido, " eres el impostor?"
+
+contrasena()
