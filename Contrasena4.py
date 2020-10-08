c81d78844d282ad7db9a905b3147fe83488fa855
0HYANGH0 <71264983+0HYANGH0@users.noreply.github.com>
2020-10-08 12:56:45 +0200

    Add files via upload

---
+++ Contrasena4.py
@@ -0,0 +1,8 @@
+'''Escribe un programa que genere contrasena'''
+def contrasena_4():
+    nombre=raw_input("Introduce el nombre: ")
+    apellido =raw_input("Introduce el apellido: ")
+    print nombre," ", apellido, "eres el impostor?"
+    print "Fue eyectado",3*nombre+3*apellido
+
+contrasena_4()
